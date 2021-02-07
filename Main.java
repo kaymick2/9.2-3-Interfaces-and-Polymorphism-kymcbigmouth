8cb7baa2e4f4770ed5503462aca43c5a80c7f194
Kyle McDowell <kymcd21@icstudents.org>
2021-02-07 02:06:41 +0000

    ur mom

--- Main.java
+++ Main.java
@@ -8,25 +8,25 @@ public class Main
       DataSet bankData = new DataSet();
 
       bankData.add(new BankAccount(0));
-      bankData.add(new BankAccount(10000));
-      bankData.add(new BankAccount(2000));
+      bankData.add(new BankAccount(20000));
+      bankData.add(new BankAccount(4000));
 
       System.out.println("Average balance: " + bankData.getAverage());
-      System.out.println("Expected: 4000");
+      System.out.println("Expected: 8000");
       Measurable max = bankData.getMaximum();
       System.out.println("Highest balance: " + max.getMeasure());
-      System.out.println("Expected: 10000");
+      System.out.println("Expected: 20000");
 
       DataSet coinData = new DataSet();
 
-      coinData.add(new Coin(0.25, "quarter"));
-      coinData.add(new Coin(0.1, "dime"));
-      coinData.add(new Coin(0.05, "nickel"));
+      coinData.add(new Coin(0.3, "quarter"));
+      coinData.add(new Coin(0.2, "dime"));
+      coinData.add(new Coin(0.1, "nickel"));
 
       System.out.println("Average coin value: " + coinData.getAverage());
-      System.out.println("Expected: 0.133");            
+      System.out.println("Expected: 0.199");            
       max = coinData.getMaximum();
       System.out.println("Highest coin value: " + max.getMeasure());
-      System.out.println("Expected: 0.25");            
+      System.out.println("Expected: 0.3");            
    }
 }
